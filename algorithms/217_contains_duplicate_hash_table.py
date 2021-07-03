c88788f3f29e6e4dd335495733bac5bd39d14269
Dominik Schauer <dominik.schauer@deutschebahn.com>
2021-07-03 20:20:01 +0200

    made solution for 217 more elegant

--- algorithms/217_contains_duplicate_hash_table.py
+++ algorithms/217_contains_duplicate_hash_table.py
@@ -1,19 +1,14 @@
 # By using a dictionary to remember each value we had seen before, 
 # we will know in case we encounter the same value a second time (i.e. it's a duplciate)
-# Obviously there's no need to go this far if there aren't at least two
-# values in the array, since then it's impossible to have duplicates. 
-
+     
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if(len(nums) < 2):
-            return False
+        num_dict = {}
         
-        value_dict = {}
-        
-        for i in range(0,len(nums)):
-            if(nums[i] in value_dict):
+        for num in nums: 
+            if num in num_dict:
                 return True
             else:
-                value_dict[nums[i]] = None
-            
+                num_dict[num] = None
+                
         return False
